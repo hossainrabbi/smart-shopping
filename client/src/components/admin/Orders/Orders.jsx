1c1815e1f49be4ce2652e875d6625ad01a7b374f
hossainrabbi <hossainrabbi259@gmail.com>
2022-11-23 22:11:07 +0600

    deploy error solved

--- client/src/components/admin/Orders/Orders.jsx
+++ client/src/components/admin/Orders/Orders.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect} from 'react';
 import { Form, Table } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
